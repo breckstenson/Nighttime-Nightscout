d56c2dd174a4f43e42cd23df370579cabad6e142
breckstenson <breckstenson@gmail.com>
2025-04-02 17:25:09 -0400

    Add files via upload

---
+++ credentials.h
@@ -0,0 +1,4 @@
+#define WIFI_SSID "SSID" //put your wifi's ssid
+#define WIFI_PASS "PASS"  //put your wifi's password. if your wifi doesn't have a password, put "" instead
+#define NS_SITE "your.nightscout.com" //put your nightscout url here without the https
+#define TIMEZONE -5 //put the hours offset for your timezone here. ie. eastern time is GMT-5
